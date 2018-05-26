f23329bcfcb647ca9b15c6ec5aee99309f0f9533
Forat Seif <foratseif@gmail.com>
2018-05-26 22:32:42 +0200

    fixed bugs in datareader.py

--- project/yolo/datareader.py
+++ project/yolo/datareader.py
@@ -22,6 +22,7 @@ def csv2frames(filepath, start_frame=None, scale=1):
     # if start_frame is set, skip to frame after
     if start_frame != None:
         for line in f:
+            data = line.split() # split lines by whitespaces
             if data[0] == start_frame:
                 break
 
@@ -78,6 +79,10 @@ def csv2frames(filepath, start_frame=None, scale=1):
         if len(tmp_arr) > 4:
             data_vector.append(tmp_arr)
 
+def plot_shit(cell, aw, ah, bw, bh):
+    #print(aw, ah, bw, bh)
+    return cell
+
 
 # turns frames and frame-labels to useful training data
 def frames2data(img, data, grid_size, anchors):
@@ -87,8 +92,12 @@ def frames2data(img, data, grid_size, anchors):
     ch = int(img_shape[0]/grid_size[0])
     cw = int(img_shape[1]/grid_size[1])
 
-    data = np.matrix(data)
+    data = np.matrix(data, np.float64)
+
+    # get anchor number
     n_anchors = anchors.shape[0]
+
+    # gem dimentions for label
     object_len = (data.shape[1]+1)
     label_len  = n_anchors * object_len
 
@@ -128,7 +137,22 @@ def frames2data(img, data, grid_size, anchors):
                 for k in range(n_anchors):
                     anch = anchors[k]
 
-                    intersec = np.min(bbox[1,3], anch[0,0]) * np.min(bbox[0,4], anch[0,1])
+                    intersec_h = 0
+                    intersec_w = 0
+
+                    #print((bbox[0,3],bbox[0,4]), (anch[0,0],anch[0,1]))
+
+                    if bbox[0,3] > anch[0,1]:
+                        intersec_w = anch[0,1]
+                    else:
+                        intersec_w = bbox[0,3]
+
+                    if bbox[0,4] > anch[0,0]:
+                        intersec_h = anch[0,0]
+                    else:
+                        intersec_h = bbox[0,4]
+
+                    intersec = intersec_h * intersec_w
                     tot_area = bbox[0,3]*bbox[0,4] + anch[0,0]*anch[0,1] - intersec
 
                     tmp_iou = intersec/tot_area
@@ -137,9 +161,9 @@ def frames2data(img, data, grid_size, anchors):
                         iou_index = k
 
                 if iou_index > -1:
-                    label[k*object_len : (k+1)*object_len] = bbox
+                    label[iou_index*object_len : (iou_index+1)*object_len] = bbox
 
-            yield cell, label
+            yield cell, label, bboxes
 
 
 # turns csv file directly into traning data
@@ -159,32 +183,73 @@ if __name__ == '__main__':
 
     path = sys.argv[1]
 
-    anchors = np.matrix([[1, 1],
-                         [0.5, 0.5]])
-
+    anchors = np.matrix([[1, 0.5],
+                         [0.5, 1],
+                         [0.7, 0.7],
+                         [1.2, 0.4],
+                         [0.1, 0.1]
+                         ], dtype=np.float64)
+    grid = (13, 13)
     key = -1
     pause = False
+
+    fack = 0
+    shit = 0
+    blank = None
+
     for img, data in csv2frames(path, None, 0.5):
 
-        for objects in data:
-            color = (np.array(objects[4:7])*255).tolist()
-            w_2 = int(objects[2]/2)
-            h_2 = int(objects[3]/2)
-            cv2.rectangle(img, (int(objects[0]) - w_2, int(objects[1]) - w_2), (int(objects[0]) + w_2, int(objects[1]) + w_2), color, 2)
-            cv2.rectangle(img, (int(objects[0]) - 1, int(objects[1]) - 1), (int(objects[0]) + 1, int(objects[1]) + 1), color, 2)
-            #img[int(objects[2]):int(objects[4]), int(objects[1]):int(objects[3])] = color
+#        for objects in data:
+#            color = (np.array(objects[4:7])*255).tolist()
+#            w_2 = int(objects[2]/2)
+#            h_2 = int(objects[3]/2)
+#            cv2.rectangle(img, (int(objects[0]) - w_2, int(objects[1]) - w_2), (int(objects[0]) + w_2, int(objects[1]) + w_2), color, 2)
+#            cv2.rectangle(img, (int(objects[0]) - 1, int(objects[1]) - 1), (int(objects[0]) + 1, int(objects[1]) + 1), color, 2)
+#            #img[int(objects[2]):int(objects[4]), int(objects[1]):int(objects[3])] = color
 
-        for x, y in frames2data(img, data, (2, 2), anchors):
 
-            cv2.imshow('data_video_show', x)
+        for x, y, bbs in frames2data(img, data, grid, anchors):
+            blank = np.copy(img)
 
             print(y.tolist())
+
+            h, w = x.shape[0], x.shape[1]
+            #print(h, w)
+            #print(x.shape)
+            #blank[(shit*h)+(shit*2):(shit*2)+((shit+1)*h), (fack*2)+(fack*w):(fack*2)+((fack+1)*w)] = x
+            blank[(shit*h):((shit+1)*h), (fack*w):((fack+1)*w)] = x*0.4
+
+            #blank_backup = np.copy(blank)
+
+            for bb in bbs:
+                color = np.array(bb[:,5:8]*255, dtype=int).tolist()[0]
+                w_2  = int(bb[0,3]/2 * w)
+                h_2  = int(bb[0,4]/2 * h)
+                xpos = int(bb[0,1] * w) + (fack*w)
+                ypos = int(bb[0,2] * h) + (shit*h)
+
+                for anch in anchors:
+                    anch_h2 = int(anch[0,0]/2*h)
+                    anch_w2 = int(anch[0,1]/2*w)
+                    cv2.rectangle(blank, (int(xpos) - anch_w2, int(ypos) - anch_h2), (int(xpos) + anch_w2, int(ypos) + anch_h2), [255,255,255], 1)
+
+                cv2.rectangle(blank, (int(xpos) - 1, int(ypos) - 1), (int(xpos) + 1, int(ypos) + 1), color, 1)
+                cv2.rectangle(blank, (int(xpos) - w_2, int(ypos) - h_2), (int(xpos) + w_2, int(ypos) + h_2), color, 1)
+
+
+
+            cv2.imshow('data_video_show', blank)
+
+            fack += 1
+            if fack == grid[1]:
+                fack = 0
+                shit = (shit+1)%grid[0]
 
             key = -1
             if pause == True:
                 key = cv2.waitKey(0)
             else:
-                key = cv2.waitKey(25)
+                key = cv2.waitKey(1)
 
             if key == 32:
                 pause = not pause
@@ -193,23 +258,7 @@ if __name__ == '__main__':
             elif key > -1:
                 cv2.destroyWindow("data_video_show")
                 exit(0)
-#
-#    for img, data in csv2frames(path, None, 0.5):
-#
-#        for fack in data:
-#            color = (np.array(fack[5:8])*255).tolist()
-#            cv2.rectangle(img, (int(fack[1]), int(fack[2])), (int(fack[3]), int(fack[4])), color, 1)
-#
-#        cv2.imshow('data_video_show', img)
-#
-#        #im_dst = cv2.warpPerspective(img.copy(), h, (600,600))
-#        #cv2.imshow('perspective', im_dst)
-#
-#        key = cv2.waitKey(25)
-#        if key > -1:
-#            break
 
-    # destroy all windows
 
 
 
